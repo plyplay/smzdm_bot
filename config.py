c8d3158356ad34bde92c65b0f2d8c11c92c8d489
plyplay <71923666@qq.com>
2023-11-26 23:22:56 +0800

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ DEFAULT_HEADERS = {
 """
 调试用 COOKIE
 """
-TEST_COOKIE = ''
+TEST_COOKIE = 'device_id=605676925164295389855803747604c4dd049da0d6f24c7b2f874291b; r_sort_type=score; smzdm_user_source=0D9A238AE7F0E1DD633E87AADA21790D; __ckguid=pT843fbBC5eAkptokJGRfE3; homepage_sug=d; _zdmA.vid=*; Hm_lvt_9b7ac3d38f30fe89ff0b8a0546904e58=1701012036; footer_floating_layer=0; ad_date=26; ad_json_feed=%7B%7D; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%226091111024%22%2C%22first_id%22%3A%2217e87aef600585-00fb1571293d31-3f3a5b06-2073600-17e87aef6017e5%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%7D%2C%22%24device_id%22%3A%2217e87aef600585-00fb1571293d31-3f3a5b06-2073600-17e87aef6017e5%22%7D; sess=BA-0qc48Z6uiZDZ%2FGyDpG37kLluDkqSHpKn%2BbcI0yH8ys032wmoADK3B6S4tOxoO1jaNhqBE%2Fbp5qmf6qPwR0%2B3zQSF5BC4vwyYWL7YzXKFdvHX%2FrwoDaZZDwQ%3D; user=user%3A6091111024%7C6091111024; smzdm_id=6091111024; _zdmA.uid=ZDMA.fXyf8bcG_.1701012052.2419200; bannerCounter=%5B%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A0%2C%22surplus%22%3A1%7D%2C%7B%22number%22%3A1%2C%22surplus%22%3A1%7D%5D; Hm_lpvt_9b7ac3d38f30fe89ff0b8a0546904e58=1701012115; _zdmA.time=1701012116077.0.https%3A%2F%2Fwww.smzdm.com%2F'
 
 
 """
